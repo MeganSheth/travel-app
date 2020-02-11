c902c6c97c86624b9e12747e9dd2139925e91c11
MeganSheth <megansheth2003@gmail.com>
2020-02-11 14:59:41 -0500

    created json and were able to print max value

--- app/src/main/java/com/example/travel360/Survey.java
+++ app/src/main/java/com/example/travel360/Survey.java
@@ -3,6 +3,7 @@ package com.example.travel360;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,11 +12,20 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class Survey extends AppCompatActivity {
 
@@ -30,21 +40,23 @@ public class Survey extends AppCompatActivity {
     }
 
     public String loadJSONFromAsset() {
+        AssetManager assetManager = getResources().getAssets();
+        InputStream inputStream = null;
         String json = null;
         try {
-            InputStream is = getAssets().open("assets/locations.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            json = new String(buffer, "UTF-8");
-            Log.d("radio", "it worked=================");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            Log.d("radio", "oops=================");
-            return null;
+            inputStream = assetManager.open("locations.json");
+            if ( inputStream != null)
+                Log.d("string", "It worked!");
+                int size = inputStream.available();
+                byte[] buffer = new byte[size];
+                inputStream.read(buffer);
+                inputStream.close();
+                json = new String(buffer, "UTF-8");
+                return json;
+            } catch (IOException e) {
+            e.printStackTrace();
         }
-        return json;
+        return "hey";
     }
 
     public void submitSurvey(View view) {
@@ -72,23 +84,75 @@ public class Survey extends AppCompatActivity {
             int radioButtonID = group.getCheckedRadioButtonId();
             RadioButton radioButton = (RadioButton) group.findViewById(radioButtonID);
             String response = radioButton.getTag().toString();
+            Log.d("string", question);
             answers.put(question, response);
-            //answers.add(radioButton.getText().toString());
-
-
-            //group1.text
-
         }
-        Log.d("radio", loadJSONFromAsset());
+        String json = loadJSONFromAsset();
+        JSONArray jsonArray = null;
 
-        Intent intent = new Intent(Survey.this, Submit.class);
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            jsonArray = jsonObject.getJSONArray("locations");
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        Map <String, Integer> output = new HashMap<String, Integer>();
+        if (jsonArray != null){
+            Log.d("string", "Array isn't empty");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                try {
+                    JSONObject explrObject = jsonArray.getJSONObject(i);
+                    String location = explrObject.getString("location");
+                    Log.d("string", location);
+                    JSONObject characteristics = explrObject.getJSONObject("characteristics");
+                    Iterator<String> iter = characteristics.keys();
+                    int count = 0;
+                    while (iter.hasNext()) {
+                        String key = iter.next();
+                        try {
+                            int characteristicValue = (int) characteristics.get(key);
+                            String answerString = answers.get(key);
+                            int answerValue = Integer.valueOf(answerString);
+                            if (characteristicValue == answerValue) {
+                                count++;
+                            }
+                        } catch (JSONException e) {
+                            Log.d("string", "oof");
+                        }
+                    }
+                    output.put(location, count);
+                    // Array(location : count) to hashtable
+                    Log.d("integar", "value: " + count);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        }
+
+        int max =  0;
+        for (Map.Entry<String, Integer> entry : output.entrySet()) {
+            if (entry.getValue() > max) {
+                max = entry.getValue();
+
+            }
+            Log.d("int", String.valueOf(max));
+            Log.d("string", String.valueOf(output));
+        }
+
+        //Intent intent = new Intent(Survey.this, Submit.class);
         //intent.putStringArrayListExtra("questions", answers);
-        startActivity(intent);
-
-
+        //startActivity(intent);
 
     }
 
+
+
+
+
 }
+
+
 
 
